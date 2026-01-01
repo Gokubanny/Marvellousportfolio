b070696a52295cf963ff3a65c54ea58847877640
Gokubanny <omatulemarvellous721@gmail.com>
2026-01-02 00:58:41 +0100

    Initial

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
   images: {
-    unoptimized: true, // Required for static export
+    unoptimized: true,
   },
-  // If you're using trailing slashes
-  trailingSlash: true,
 }
 
 module.exports = nextConfig
